ae328882859c32e3ccdc429976c12e9afbd6fe76
arttunorja <arttu.norja@gmail.com>
2024-02-15 10:19:41 +0200

    fixed qr camera

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
 import { useEffect, useState } from 'react';
-import Home from './Home';
-import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 function App() {
-  const [scanResult, setScanResult] = useState(null)
-
+  const [scanResult, setScanResult] = useState(null);
 
   useEffect(() => {
-      const scanner = new Html5QrcodeScanner('reader', {
-        qrbox: {
-          width: 250,
-          height:250
-        },
-        fps: 5,
-      })
-    
-      scanner.render(success, error);
-    
-    function success (result) {
-      scanner.clear()
-      setScanResult(result)
-    }
-    
-    
-    function error (err) {
-      console.warn(err)
-    }
-  }, [])
+    const scanner = new Html5QrcodeScanner('reader', { fps: 10, qrbox: { width: 250, height: 250 } }, false);
+    const onScanSuccess = (decodedText, decodedResult) => {
+      scanner.clear().then(() => {
+        setScanResult(decodedText);
+      }).catch((err) => console.warn(err));
+    };
+    const onScanFailure = (error) => {
+      console.warn(error);
+    };
+
+    scanner.render(onScanSuccess, onScanFailure);
+
+    return () => {
+      scanner.clear().catch((err) => console.warn(err));
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <div className="content">
-        { scanResult
-        ? <div>Success: {scanResult}</div>
-        : <div id="reader"></div>
-        }
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={scanResult ? <div>Success: {scanResult}</div> : <div id="reader"></div>} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-
+    </Router>
   );
 }
 
